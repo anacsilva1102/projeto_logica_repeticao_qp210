5832420b9462f7ba20b010e239554bf206d436f8
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-06-16 16:43:40 -0300

    alterar arquivo

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -3,7 +3,7 @@ const divFlor = document.querySelector('#div-for')
 
 for(i = 0;i < 10; i++ ){
     console.log(i, 'Amor')
-    divFor.innerHTML += `${i} - Amor <br> `
+    divFlor.innerHTML += `${i} - Amor <br> `
 }
   //CONTADOR/ACUMULADOR
   const inputNum = document.querySelector('#num')
