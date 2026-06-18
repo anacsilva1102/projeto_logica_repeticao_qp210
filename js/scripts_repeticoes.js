57c59cd8c97e4f4183d665a1de4f6c89384f615b
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-06-18 13:31:58 -0300

    alterar arquivo

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -27,67 +27,66 @@ btnNum.addEventListener('click' , (evet)=>{
 
 })
 
-// CONTROLANDO FOR
+//CONTROLADOR FOR
 const inputFrase = document.querySelector('#frase')
 const inputNumRepeticao = document.querySelector('#num-repeticao')
 const btnFrase = document.querySelector('#btn-frase')
 const divFrase = document.querySelector('#div-frase')
 
-btnFrase.addEventListener('click', (evt) =>{
-  let numRepeti = Number(inputNumRepeticao.value)
-  let frase = inputFrase.value
+btnFrase.addEventListener('click' , (evt) => {
+    let numRepeti = Number(inputNumRepeticao.value)
+    let frase = inputFrase.value
 
-  for(i = 0; i < numRepeti; i++){
-    divFrase.innerHTML += `${i + 1} - ${frase} <br>`}
+    for (i = 0;i < numRepeti; i++){
+        divFrase.innerHTML += `${i + 1} - ${frase} <br>`
+    }
 })
 
 //COLEÇÃO DE DADOS - ARRAY
-const presentes = ['Bicicleta', 'Celular', 'Camisa do Mengão', 'Doleira', 'Tênis Adidas']
+const presentes = ['Bicicleta', 'Celular', 'Camisa do Mengão', 'Doleira', 'tênis Adidas']
 
 const divArray = document.querySelector('#div-array')
 
-//divArray.innerHTML = presentes[6]
- 
-for(i = 0;i < 5;i++){ 
-  divArray.innerHTML += `${presentes[i]} <br>`
+//divArray.innerHtml = presentes[6]
 
+for(i = 0;i < 5;i++ ){
+    divArray.innerHTML += `${presentes[i]} <br>`
 }
 
 //FOR IN
 const divForIn = document.querySelector('#div-forin')
 
 for(let posicao in presentes){
-  divForIn.innerHTML += `${presentes[posicao]}`
+ divForIn.innerHTML += `${presentes[posicao]} <br>`
 }
 
 //FOR OF
 const divForOf = document.querySelector('#div-forof')
 
 for (let elemento of presentes){
-   divForOf.innerHTML += `${elemento} <br>`
+    divForOf.innerHTML += `${elemento} <br>`
 }
 
 //FOREACH
 const divForeach = document.querySelector('#div-foreach')
 
 presentes.forEach((elemento, posicao)=>{
-  divForeach.innerHTML += `${posicao} - ${elemento} <br`
-
+    divForeach.innerHTML += `${posicao + 1} - ${elemento} <br>`
 })
+
 //COLEÇÃO DE OBJETOS LITERAIS - ARRAY
 const pessoas = [
-  {nome: 'Maria Flor', idade:25, renda:8500},
-  {nome: 'joerdson Souza', idade:75, renda:5000},
-  {nome: 'Taoca', idade: 28, renda:500},
-  {nome:'Chicó', idade: 36, renda:100},
-  {nome:'João Grilo', idade: 32, renda:80},
+    {nome: 'Maria Flor', idade: 25, renda:8500},
+    {nome: 'Joerdson Souza', idade: 75, renda:5000},
+    {nome: 'Taoca', idade: 28, renda:500},
+    {nome: 'Chicó', idade: 36, renda:100},
+    {nome: 'Chicó', idade: 36, renda:100},
+    {nome: 'João Grilo', idade: 32, renda:80},
 ]
 
-//LISTANDO OBJETOS LITERAL PELO FOR 
-const divListaObjFor= document.querySelector('#div-listaobj-for')
+//LISTA OBJETO LITERAL PELO FOR
+const divlistaObjFor = document.querySelector('#div-listaobj-for')
 
-for(i = 0;i < pessoas.length;i++){
-  divListaObjFor.innerHTML += `${pessoas[i].nome}, ${pessoas[i].idade} R$ ${pessoas[i].renda.toFixed(2).replace('.',',')} <br>`
-}
-
+for(i = 0; i < pessoas.length;i++){
+    divlistaObjFor.innerHTML += `${pessoas[i].nome}, ${pessoas[i].idade} R$ ${pessoas[i].renda.toFixed(2).replace('.', ',')} <br>`}
 
