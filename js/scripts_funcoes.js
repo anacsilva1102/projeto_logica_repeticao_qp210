66a3a00bde2dcbb978893c4262d0afcd8c131ffb
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-06-19 14:22:12 -0300

    alterar arquivo

---
+++ js/scripts_funcoes.js
@@ -0,0 +1,45 @@
+
+function mensagem(){
+    console.log('uma mensagem')
+}
+
+mensagem()
+
+function mensagemLinda(txt){
+    console.log(txt)
+}
+
+mensagemLinda('Brasil vai dar um pau no Haiti')
+
+function somaNumeros(num1, num2){
+    return num1 + num2
+}
+
+console.log('As soma dos números', somaNumeros(10, 58))
+
+const multiplicacao = function(num1, num2){
+    return num1 * num2
+}
+
+console.log(multiplicacao(64, 5))
+
+console.log('-------> ARROW FUNCTION <-------')
+
+const msgArrow = () => {
+    console.log('outra mensagem na aroow function')
+}
+
+msgArrow()
+
+const divisaoNum = (valor1, valor2) => {
+     let resposta
+
+    if ((valor1 > 0) && (valor2 >0))
+    { resposta = valor1 / valor2}else{
+            resposta = ('NÃO É POSSIVEL DIVISÃO POR ZERO')
+        }
+        return resposta
+    }
+
+
+    console.log(divisaoNum(10,8000))
