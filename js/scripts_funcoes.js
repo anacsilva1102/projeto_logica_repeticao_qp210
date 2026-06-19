d607f0d06e4a344ca474879c9ed4b04badae34f1
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-06-19 15:51:40 -0300

    alterar arquivo

--- js/scripts_funcoes.js
+++ js/scripts_funcoes.js
@@ -43,3 +43,58 @@ const divisaoNum = (valor1, valor2) => {
 
 
     console.log(divisaoNum(10,8000))
+
+    console.log(Math.random())
+
+    console.log('o número sortado ===>',parseInt(Math.random() * 100))
+
+    setTimeout(()=>{
+        console.log('Executou após 5s')
+        msgArrow()
+    }, 5000)
+
+   /* let cont = 0
+    const intervalo = setInterval(()=>{
+    cont++
+     console.log(cont, 'Repetição da setInterval')
+
+
+     if(cont == 5){
+        clearInterval()
+    
+     }
+    }, 3000)*/
+
+    const divNumSorteio = document.querySelector
+    ('#div-num-sorteio')
+
+    let contVolta = 0, numSorteado = 0, contPar = 0, contImpar = 0
+
+    const sorteio = setInterval(()=>{
+        contVolta++
+
+        numSorteado = parseInt(Math.random() * 60)
+
+        console.log(`${contVolta}º Número sorteado ${numSorteado}`)
+
+        if (numSorteado % 2 == 0) {
+            contPar++
+        }else{
+            contImpar++
+        }
+
+        divNumSorteio.innerHTML += `${contVolta}º número gerado ${numSorteado} - ${numSorteado % 2 == 0 ? 'PAR' :'IMPAR'} <br>`
+
+        if(contVolta == 6) {
+            console.log('-------> SORTEIO ENCERRADO <-------')
+            
+            divNumSorteio.innerHTML += `-------> SORTEIO ENCERRADO <------- <br>
+            TOTAL DE NÚMEROS GERADOS: ${contVolta} <br>
+            TOTAL DE NÚMEROS PAR: ${contPar} <br>
+            TOTAL DE NÚMEROS ÍMPAR: ${contImpar}`
+
+            clearInterval(sorteio)
+            contVolta = 0
+            numSorteado = 0
+        }
+    }, 200)
